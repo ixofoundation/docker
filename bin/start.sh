9af9c845bf613c678b9d2cb2ccebe0d70c16ef0a
source-code-smith <herman@sourcecodesmith.com>
2018-11-07 14:59:21 +0200

    tweaks for local BC other services on docker hybrid

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+IP_ADDRESS=$1
+if [ -z "$IP_ADDRESS" ];
+then
+  echo "YOU HAVE TO SUPPLY AN IP ADDRESS FOR THE BLOCKCHAIN"
+  exit 1
+fi
+export IP_ADDRESS=$IP_ADDRESS
+
 #green=`tput setaf 2`
 echo "***********************************"
 echo "* DOCKER START                    *"
@@ -12,14 +20,12 @@ docker-compose -f "$ROOT_DIR/docker-compose.yml" up --no-start
 
 # docker-compose create
 docker-compose start db
-docker-compose start blockchain
 docker-compose start mq
 docker-compose start cache
 
 # attempting to wait for mongodb to be ready
 $ROOT_DIR/bin/wait-for-service.sh db 'waiting for connections on port' 10
 $ROOT_DIR/bin/wait-for-service.sh mq 'Server startup complete;' 10
-docker-compose start rest
 docker-compose start block-sync
 docker-compose start ixo-web
 
@@ -32,4 +38,3 @@ echo "***********************************"
 echo "* DOCKER START COMPLETE           *"
 echo "***********************************"
 docker-compose ps
-# branch: dev
